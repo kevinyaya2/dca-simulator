537ccc5b613660d1ed1114a95d0e4a8fa38fbef8
Kevin Liao <a5011841118@gmail.com>
2025-12-17 17:35:53 +0800

    優化架構

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import DCA from "./pages/DCA";
-import InvestmentGame from "./pages/InvestmentGame";
-import CardGame from "./pages/CardGame";
-import FightGame from "./pages/FightGame";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import routes from "./pages/routes";
 import "./App.css";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dca" element={<DCA />} />
-        <Route path="/game" element={<InvestmentGame />} />
-        <Route path="/cards" element={<CardGame />} />
-        <Route path="/fight" element={<FightGame />} />
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
       </Routes>
     </Router>
   );
---
+++ src/pages/routes.jsx
@@ -0,0 +1,36 @@
+import Home from "./Home";
+import DCA from "./DCA";
+import InvestmentGame from "./InvestmentGame";
+import CardGame from "./CardGame";
+import FightGame from "./FightGame";
+
+// 路由配置：集中管理所有頁面路由
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+    title: "首頁",
+  },
+  {
+    path: "/dca",
+    element: <DCA />,
+    title: "定期定額模擬器",
+  },
+  {
+    path: "/game",
+    element: <InvestmentGame />,
+    title: "投資遊戲",
+  },
+  {
+    path: "/cards",
+    element: <CardGame />,
+    title: "投資卡牌",
+  },
+  {
+    path: "/fight",
+    element: <FightGame />,
+    title: "柴剛格鬥",
+  },
+];
+
+export default routes;
